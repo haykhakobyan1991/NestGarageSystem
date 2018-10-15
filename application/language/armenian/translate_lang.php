e9042ad8557b6f88f407c80ab22458b587028473
tilema <tigranmkrtchyna.97@gmail.com>
2018-10-15 21:06:41 +0400

    TIKO

---
+++ application/language/armenian/translate_lang.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['News'] = 'Նորություններ';
+$lang['Serials'] = 'Սերիալներ';
+$lang['sign_in'] = 'Մուտք';
+$lang['register'] = 'Գրանցվել';
+$lang['sign_in_with'] = 'Մուտք գործել';
+$lang['first_name'] = 'Անուն';
+$lang['last_name'] = 'Ազգանուն';
+$lang['email'] = 'Էլ․ հասցե';
+$lang['login'] = 'Մուտքանուն';
+$lang['email_or_login'] = 'Էլ․ հասցե կամ մուտքանուն';
+$lang['birthday'] = 'Ծննդյան օր';
+$lang['password'] = 'Գաղտնաբառ';
+$lang['last_news'] = 'Վերջին Նորությունները';
+$lang['last_series'] = 'Վերջին սերիաները';
+$lang['email_is_not_unique'] = 'Այս էլ․ հասցեն արդեն գրանցված է';
+$lang['username_is_not_unique'] = 'Այս մուտքանունը արդեն գոյություն ունի';
+$lang['field_required'] = 'Այս դաշտը պարտադիր է';
+$lang['not_valid_email'] = 'Մուտքագրեք վավեր էլ․ հասցե';
+$lang['minimum_6_characters'] = 'նվազագույնը 6 նիշ';
+$lang['success_reg'] = 'Ակտիվացման հարցումը ուղարկվել է<br />Գրանցման պահանջը ընդունվել է:<br /><br />Կայաքի ադմինիստրացիան պահանջում է բոլոր էլ. հայացքի իրականությունը 10 րոպե հետո (հնարավոր է ավելի վաղ) դուք կստանաք էլեկտրոնային նամակ, հաջորդ քայլի հրահանգներով:';
+$lang['my_profile'] = 'Իմ էջը';
+$lang['logout'] = 'Դուրս գալ';
+
+
---
+++ application/language/russian/translate_lang.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$lang['News'] = 'Новости';
+$lang['Serials'] = 'Сериалы';
+$lang['sign_in'] = 'Вход';
+$lang['register'] = 'Регистрациа';
+$lang['sign_in_with'] = 'Войти в систему с';
+$lang['first_name'] = 'Имя';
+$lang['last_name'] = 'Фамоля';
+$lang['email'] = 'Эл. адрес';
+$lang['login'] = 'Имя пользователя';
+$lang['email_or_login'] = 'Эл. адрес или Имя пользователя';
+$lang['birthday'] = 'День рождения';
+$lang['password'] = 'Пароль';
+$lang['last_news'] = 'Последние новости';
+$lang['last_series'] = 'ПОСЛЕДНИЕ СЕРИИ';
+$lang['email_is_not_unique'] = 'Этот Эл. адрес уже зарегистрирован';
+$lang['username_is_not_unique'] = 'Этот логин уже существует';
+$lang['field_required'] = 'Это поле обязательно для заполнения';
+$lang['not_valid_email'] = 'Введите действительный адрес электронной почты.';
+$lang['minimum_6_characters'] = 'Минимум 6 символов';
+$lang['success_reg'] = 'Отправлен запрос на активацию<br />Запрос на регистрацию принят.<br /><br />Администрация сайта требует реальности всех вводимых e-mail-адресов. Через 10 минут (возможно и раньше) Вы получите письмо с инструкциями для следующего шага';
+$lang['my_profile'] = 'Мой профайл';
+$lang['logout'] = 'Выйти';

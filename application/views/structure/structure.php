ed9487053f88bf279b46c0d6d9904a342e87c87d
hayk <haikhakobyan2@gmail.com>
2018-10-29 19:00:26 +0400

    Hayk

--- application/views/structure/structure.php
+++ application/views/structure/structure.php
@@ -8,18 +8,32 @@
 		display: block;
 		margin: 0 auto;
 	}
+	tbody {
+		transform: scale(0.9);
+	}
 </style>
+<link href="<?= base_url('assets/css/jquery.ui.min.css') ?>"/>
 
 
 <div class="jumbotron jumbotron-fluid pb-2 pt-2">
 	<div class="">
 		<p class="display-5 font-weight-bold mb-0 pl-3">Structure</p>
 	</div>
+	<div id="dr"
+		 style="border: 1px solid #000;max-width: 800px;max-height: 600px;overflow: hidden;cursor:grab;-moz-user-select: none; -webkit-user-select: none; -ms-user-select:none; user-select:none;-o-user-select:none;position: relative;"
+		 unselectable="on" onselectstart="return false;" onmousedown="return false;">
+		<div id="chart_div" style="z-index: 999;"></div>
+		<div style="position:absolute; left: 0;bottom: 0;padding:3px;background: #8e8f902e;">
+			<p style="font-size: 10px;margin:0;">For default scale press Ctrl + 0</p>
+			<p style="font-size: 10px;margin:0;">For Zoom In Ctrl + Mouse Scroll Down</p>
+			<p style="font-size: 10px;margin:0;">For Zoom Out Ctrl + Mouse Scroll Up</p>
+		</div>
+	</div>
 
-	<div id="chart_div" style="border: 1px solid #000;max-width: 600px;max-height: 400px;overflow: hidden;"></div>
 </div>
 
 <script src="<?= base_url('assets/js/google_chart.js') ?>"></script>
+<script src="<?= base_url('assets/js/jquery.ui.min.js') ?>"></script>
 
 <script type="text/javascript">
 	google.charts.load('current', {packages: ["orgchart"]});
@@ -66,7 +80,15 @@
 <script>
 	var zoomable = document.getElementById('chart_div');
 	var content = document.getElementById('chart_div').getElementsByTagName('tbody');
-	var zX = 1;
+	var zX = 0.9;
+	$('body').on('keydown', function (e) {
+		if (e.ctrlKey && e.keyCode == 48) {
+			$('tbody').css('transform', 'scale(0.9)');
+			$('#chart_div').css({'left': '0', 'top': '0'});
+			zX = 0.9;
+		}
+	});
+
 	window.addEventListener('wheel', function (e) {
 		var dir;
 		if (!e.ctrlKey) {
@@ -74,6 +96,7 @@
 		}
 		dir = (e.deltaY > 0) ? 0.1 : -0.1;
 		zX += dir;
+
 		for (var i = 0; i < content.length; i++) {
 			zoomable.getElementsByTagName('tbody')[i].style.transform = 'scale(' + zX + ')';
 		}
@@ -81,4 +104,10 @@
 		e.preventDefault();
 		return;
 	});
+
+
+	$(document).ready(function () {
+		$("#chart_div").draggable();
+	});
+
 </script>

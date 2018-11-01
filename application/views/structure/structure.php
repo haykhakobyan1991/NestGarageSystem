5abdd7e4a51fa9ee57480d2544f35492540fff30
hayk <haikhakobyan2@gmail.com>
2018-11-01 11:33:58 +0400

    Hayk

--- application/views/structure/structure.php
+++ application/views/structure/structure.php
@@ -21,28 +21,114 @@
 				<input type="search" id="mySearch" onkeypress="if (event.keyCode === 13) searchDiagram()">
 				<button onclick="searchDiagram()">Search</button>
 			</div>
-			<textarea id="mySavedModel" style="display:none;width:100%;height:250px">{ "class": "go.TreeModel",
-		  "nodeDataArray": [
-				{"key":1, "name":"Stella Payne Diaz", "title":"CEO"},
-				{"key":2, "name":"Luke Warm", "title":"VP Marketing/Sales", "parent":1},
-				{"key":3, "name":"Meg Meehan Hoffa", "title":"Sales", "parent":2},
-				{"key":4, "name":"Peggy Flaming", "title":"VP Engineering", "parent":1},
-				{"key":5, "name":"Saul Wellingood", "title":"Manufacturing", "parent":4},
-				{"key":6, "name":"Al Ligori", "title":"Marketing", "parent":2},
-				{"key":7, "name":"Dot Stubadd", "title":"Sales Rep", "parent":3},
-				{"key":8, "name":"Les Ismore", "title":"Project Mgr", "parent":5},
-				{"key":9, "name":"April Lynn Parris", "title":"Events Mgr", "parent":6},
-				{"key":10, "name":"Xavier Breath", "title":"Engineering", "parent":4},
-				{"key":11, "name":"Anita Hammer", "title":"Process", "parent":5},
-				{"key":12, "name":"Billy Aiken", "title":"Software", "parent":10},
-				{"key":13, "name":"Stan Wellback", "title":"Testing", "parent":10},
-				{"key":14, "name":"Marge Innovera", "title":"Hardware", "parent":10},
-				{"key":15, "name":"Evan Elpus", "title":"Quality", "parent":5},
-				{"key":16, "name":"Lotta B. Essen", "title":"Sales Rep", "parent":3},
-				{"key":17, "name":"Hayk", "title":"Hakobyan", "parent":3}
-			 ]
-			}
-    </textarea>
+			<textarea id="mySavedModel" style="display:none;width:100%;height:250px">
+				{
+					"class": "go.TreeModel",
+		  			"nodeDataArray": [
+						{
+							"key":1,
+							"name":"Stella Payne Diaz",
+							"title":"CEO"
+						},
+						{
+							"key":2,
+							"name":"Luke Warm",
+							"title":"VP Marketing/Sales",
+							"parent":1
+						},
+						{
+							"key":3,
+							"name":"Meg Meehan Hoffa",
+							"title":"Sales",
+							"parent":2
+						},
+						{
+							"key":4,
+							"name":"Peggy Flaming",
+							"title":"VP Engineering",
+							"parent":1
+						},
+						{
+							"key":5,
+							"name":"Saul Wellingood",
+							"title":"Manufacturing",
+							"parent":4
+						},
+						{
+							"key":6,
+							"name":"Al Ligori",
+							"title":"Marketing",
+							"parent":2
+						},
+						{
+							"key":7,
+							"name":"Dot Stubadd",
+							"title":"Sales Rep",
+							"parent":3
+						},
+						{
+							"key":8,
+							"name":"Les Ismore",
+							"title":"Project Mgr",
+							"parent":5
+						},
+						{
+							"key":9,
+							"name":"April Lynn Parris",
+							"title":"Events Mgr",
+							"parent":6
+						},
+						{
+							"key":10,
+							"name":"Xavier Breath",
+							"title":"Engineering",
+							"parent":4
+						},
+						{
+							"key":11,
+							"name":"Anita Hammer",
+							"title":"Process",
+							"parent":5
+						},
+						{
+							"key":12,
+							"name":"Billy Aiken",
+							"title":"Software",
+							"parent":10
+						},
+						{
+							"key":13,
+							"name":"Stan Wellback",
+							"title":"Testing",
+							"parent":10
+						},
+						{
+							"key":14,
+							"name":"Marge Innovera",
+							"title":"Hardware",
+							"parent":10
+						},
+						{
+							"key":15,
+							"name":"Evan Elpus",
+							"title":"Quality",
+							"parent":5
+						},
+						{
+							"key":16,
+							"name":"Lotta B. Essen",
+							"title":"Sales Rep",
+							"parent":3
+						},
+						{
+							"key":17,
+							"name":"Hayk",
+							"title":"Hakobyan",
+							"parent":3
+						}
+			 		]
+				}
+    	</textarea>
 		</div>
 	</div>
 
@@ -418,9 +504,9 @@
 	* Document Ready Function
 	*
 	 */
+
 	$(document).ready(function () {
 		init();
-
 
 		myDiagram.addDiagramListener("ObjectSingleClicked",
 			function (e) {
@@ -448,7 +534,6 @@
 				});
 				console.table(new_arr);
 			});
-
 
 	});
 </script>
